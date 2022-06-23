000f70b2cb771914f237997e2ce9f7fdc176dbbb
Gl4ss47 <58671304+Gl4ss47@users.noreply.github.com>
2022-06-23 08:30:19 +0200

    Add files via upload

---
+++ front/js/script.js
@@ -0,0 +1,48 @@
+fillSection();
+
+/**recup api */
+async function getArticles() {
+    var articlesCatch = await fetch("http://localhost:3000/api/products")
+    return articlesCatch.json();
+}
+
+    /**  Répartition des données de l'API dans le DOM **/
+function fillSection() {
+    var result = getArticles ()
+    .then(function (resultatAPI){
+        const articles = resultatAPI;
+        console.table(articles);
+        for (let article in articles) {
+
+            /**recup du lien */
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+
+            /**recup de l'article */
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            /**recup de l'image */
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+            /**recup du titre **/
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+            /**recup du paragraphe */
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
+        }
+    })
+    .catch (function(error){
+        return error;
+    });
+}
